36dcc32c012e8d2c140f2a37ca8efd9a52ef4ca2
Borges20201248 <luisborgescarvalho2020@gmail.com>
2026-06-09 21:25:59 -0300

    parte  visual quase funcionando falta o scrpt

--- login.php
+++ login.php
@@ -3,11 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BORGESVILLE | Acesso</title>
-    <link rel="stylesheet" href="style.css?v=16.0">
+    <title>BORGESVILLE | Acesso</title> 
 </head>
-<body> 
-    
+<body>
+
     <nav class="navbar">
         <div class="header-left">
             <h1 class="brand-name">BORGESVILLE</h1>
@@ -22,17 +21,14 @@
         <a href="login.php" class="btn-login active">LOGIN / REGISTRO</a>
     </nav>
 
-    
     <div class="login-wrapper">
         <div class="auth-container">
             
-        
             <div class="auth-tabs">
                 <button class="tab-btn active" onclick="switchTab('login')">Entrar</button>
                 <button class="tab-btn" onclick="switchTab('register')">Registrar</button>
             </div>
 
-            
             <form id="form-login" class="auth-form active" action="" method="POST">
                 <div class="input-group">
                     <label for="login-email">E-mail</label>
@@ -42,30 +38,27 @@
                     <label for="login-senha">Senha</label>
                     <input type="password" id="login-senha" name="senha" placeholder="••••••••" required>
                 </div>
-                <button type="submit" class="btn-auth">Entrar</button>
+                <button type="submit" name="btn_login" class="btn-auth">Entrar</button>
             </form>
 
-        
             <form id="form-register" class="auth-form" action="" method="POST">
                 <div class="input-group">
                     <label for="reg-nome">Nome Completo</label>
-                    <input type="text" id="reg-nome" name="nome" placeholder="Seu Nome" required>
+                    <input type="text" id="reg-nome" name="nome_registro" placeholder="Seu Nome Completo" required>
                 </div>
                 <div class="input-group">
                     <label for="reg-email">E-mail</label>
-                    <input type="email" id="reg-email" name="email" placeholder="seu@email.com" required>
+                    <input type="email" id="reg-email" name="email_registro" placeholder="seu@email.com" required>
                 </div>
                 <div class="input-group">
                     <label for="reg-senha">Senha</label>
-                    <input type="password" id="reg-senha" name="senha" placeholder="Nova senha" required>
+                    <input type="password" id="reg-senha" name="senha_registro" placeholder="Crie uma nova senha" required>
                 </div>
-                <button type="submit" class="btn-auth">Criar Conta</button>
+                <button type="submit" name="btn_register" class="btn-auth">Criar Conta</button>
             </form>
 
         </div>
     </div>
-
-    
 
 </body>
 </html>
