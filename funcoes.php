0ce30d2625a5c576f03fac8d2498a16ff451d751
Borges20201248 <luisborgescarvalho2020@gmail.com>
2026-06-20 16:12:49 -0300

    arrumando funçoes e akterando as

---
+++ funcoes.php
@@ -0,0 +1,9 @@
+<?php
+
+function processarEFiltrarProdutos($pdo, $id_categoria) {
+    $sql = "SELECT * FROM produtos WHERE id_categoria = :id_categoria";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([':id_categoria' => $id_categoria]);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+?>
